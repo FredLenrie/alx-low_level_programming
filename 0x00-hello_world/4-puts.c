536dc679fead2d73a045f1ae63048d4ba3730be9
FredLenrie <fredlenrieb17@gmail.com>
2023-08-10 10:52:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a sentence using the puts function.
+ * Return: 0 as success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

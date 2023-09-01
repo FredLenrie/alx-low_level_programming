e7d9cd765fc2e560c36f655f723f5a6164bfa602
FredLenrie <fredlenrieb17@gmail.com>
2023-09-01 08:31:32 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 int main(int __attribute__((__unused__)) argc, char *argv[]);
-
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(char c);
 #endif

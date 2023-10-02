b781ec33ad089d5e0af2f51cc8d6af22d5cfc3dd
FredLenrie <fredlenrieb17@gmail.com>
2023-10-02 07:13:58 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, num_letters;
+	int o, w, num_letters = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (o == -1)
-		return (-1);
-
 	if (text_content != NULL)
-		text_content = "";
+	{
+		for (num_letters = 0; text_content[num_letters];)
+			num_letters++;
+	}
 
-	for (num_letters = 0; text_content[num_letters]; num_letters++)
-
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, num_letters);
 
-	if (w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
